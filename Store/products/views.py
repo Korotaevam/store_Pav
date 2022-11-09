43418e2fc815b43f9a4c407a90b9e2e441e0b805
Korotaevam <kam888@mail.ru>
2022-11-09 13:46:14 +0300

    base.html

--- Store/products/views.py
+++ Store/products/views.py
@@ -9,3 +9,25 @@ def index(request):
 
 def products(request):
     return render(request, 'products/products.html')
+
+
+def test_context(request):
+    context = {
+        'title': 'Store',
+        'header': 'Добро пожаловать',
+        'username': 'ivan ivanovich',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090.00},
+            {'name': 'Синяя куртка The North Face', 'price': 23725.00},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390.00},
+        ],
+        'promotion': True,
+        'products_of_promotion' :
+            [{'name': 'Черный рюкзак Nike Heritage', 'price': 2340.00},
+
+            ]
+
+
+
+    }
+    return render(request, 'products/test-context.html', context)
