32e0a71d48f1eb797dfb44ea6cbff71ef09a4bd1
Korotaevam <kam888@mail.ru>
2022-11-11 12:48:02 +0300

    start app users утв

--- Store/Store/settings.py
+++ Store/Store/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
+
