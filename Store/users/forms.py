19b60dce5c8a5a8246bdc52f390d9fdebb1e147f
Korotaevam <kam888@mail.ru>
2022-11-12 16:06:50 +0300

    login form

---
+++ Store/users/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from users.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': "Введите имя пользователя"}))
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "Введите пароль"}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': "Введите имя"}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': "Введите фамилию"}))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': "Введите имя пользователя"}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': "Введите email"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "Введите пароль"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "Введите пароль"}))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
--- Store/users/views.py
+++ Store/users/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from users.forms import *
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 
 def register(request):
-    return render(request, 'users/register.html')
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+        else:
+            print(form.errors)
+    else:
+        form = UserRegistrationForm()
+    context = {"form": form}
+    return render(request, 'users/register.html', context)
+
+
+
+
+
